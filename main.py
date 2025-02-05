9797f79280d9f8540c3bc7fca27a6ee1a9ba7e0c
sasha <08alexspir@gmail.com>
2025-02-05 18:28:00 +0600

    .

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class Funny_Student():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
-
+    b = 'ыыы'
 
 if __name__ == "__main__":
     Funny_Student()
