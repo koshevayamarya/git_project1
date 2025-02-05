e9d5d7c26fea71643b2968e52e5f5568e885b731
sasha <08alexspir@gmail.com>
2025-02-05 18:20:53 +0600

    создание холста, переменной счета и добавление изображений героя, проигрыша и приза(школьника, двойки, пятерки)

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+
+
+class Funny_Student():
+    def __init__(self):
+        pygame.init()
+        self.screen_width = 600
+        self.screen_height = 600
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption("Веселый школьник")
+        self.clock = pygame.time.Clock()
+        self.student_pos = [self.screen_width // 2, self.screen_height - 30]
+        self.score = 0
+        self.font = pygame.font.SysFont("Arial", 24)
+        self.five_image = pygame.image.load("five.png")
+        self.two_image = pygame.image.load("two.png")
+        self.student_image = pygame.image.load("student.png")
+
+        self.run()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+
+if __name__ == "__main__":
+    Funny_Student()
