d423f5d10e803fceddf9df30433be248776ec030
sasha <08alexspir@gmail.com>
2025-02-25 15:31:53 +0600

    merging to the main branch

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import random
+
 import pygame
 
 
@@ -19,12 +21,18 @@ class Funny_Student():
         self.student_image = pygame.image.load("student.png")
 
         self.student_size = self.student_image.get_size()
+        self.two_size = self.two_image.get_size()
+
+        self.two_pos = []
+        self.five_pos = []
 
         self.run()
-        n = 111
+        self.times = 0
 
     def run(self):
         while True:
+            self.times += 1
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -42,6 +50,21 @@ class Funny_Student():
                     elif event.key == pygame.K_DOWN:
                         if self.student_pos[1] + 20 + self.student_size[1] <= self.screen_height:
                             self.student_pos[1] += 20
+
+            for i in range(len(self.two_pos)):
+                self.two_pos[i][1] += 3
+
+            for i in range(len(self.five_pos_pos)):
+                self.five_pos[i][1] += 3
+
+            if self.times % 10 == 0:
+                x = random.randint(0, self.screen_width - self.two_size[0])
+                num = random.randint(0, 1)
+                if num == 0:
+                    self.two_pos.append([x, 0])
+                else:
+                    self.five_pos.append([x, 0])
+
 
 
 if __name__ == "__main__":
