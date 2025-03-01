d7e9e8a3dbf83fd73d5dce6c4bdbdda9316864ca
sasha <08alexspir@gmail.com>
2025-03-01 17:38:20 +0600

    готово!!!

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from random import random
+import random
+
 
 import pygame
 
 
 class Funny_Student():
     def __init__(self):
+        self.times = 0
+        self.speed = 1
         pygame.init()
         self.screen_width = 600
         self.screen_height = 600
@@ -18,7 +21,7 @@ class Funny_Student():
 
         self.five_image = pygame.image.load("five.png")
         self.two_image = pygame.image.load("two.png")
-        self.student_image = pygame.image.load("student.png")
+        self.student_image = pygame.image.load("hero.png")
 
         self.student_size = self.student_image.get_size()
         self.two_size = self.two_image.get_size()
@@ -27,12 +30,15 @@ class Funny_Student():
         self.five_pos = []
 
         self.run()
-        self.times = 0
+
+
 
     def run(self):
         while True:
             self.times += 1
-
+            if self.times % 200==0:
+                self.speed += 1
+                self.screen.fill((0+self.times/10, 0, 0))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -52,20 +58,60 @@ class Funny_Student():
                             self.student_pos[1] += 20
 
             for i in range(len(self.two_pos)):
-                self.two_pos[i][1] += 3
+                self.two_pos[i][1] += self.speed
 
-            for i in range(len(self.five_pos_pos)):
-                self.five_pos[i][1] += 3
+            for i in range(len(self.five_pos)):
+                self.five_pos[i][1] += self.speed
 
-            if self.times % 10 == 0:
-                x = random.randint(0, self.screen_width - self.two_size[0])
-                num = random.randint(0, 1)
+            if self.times % (100//self.speed) == 0:
+                x = random.randrange(0, (self.screen_width - self.two_size[0]), 1)
+                num = random.randrange(0, 2)
                 if num == 0:
                     self.two_pos.append([x, 0])
                 else:
                     self.five_pos.append([x, 0])
 
+            for position5 in self.five_pos:
+                if abs(position5[0] - self.student_pos[0]) <= self.student_size[0]:
+                    if abs(position5[1] - self.student_pos[1]) <= self.student_size[1]:
+                        self.score += 1
+                        self.screen.fill((0, 255, 0))
+                        self.five_pos.remove(position5)
+                else:
+                    if position5[1] >= self.screen_height:
+                        self.five_pos.remove(position5)
+
+            for position2 in self.two_pos:
+                if abs(position2[0] - self.student_pos[0]) <= self.student_size[0]/2:
+                    if abs(position2[1] - self.student_pos[1]) <= self.student_size[1]/2:
+                        message_scr = self.font.render(f"конец игры! Вы получили {self.score} пятерок", True, (255, 0, 0))
+                        self.screen.blit(message_scr, (self.screen_width // 2 - message_scr.get_width() // 2,
+                                                           self.screen_height // 2 - message_scr.get_height() // 2))
+                        pygame.display.update()
+                        pygame.time.wait(3000)
+                        pygame.quit()
+                        exit()
+                else:
+                    if position2[1] >= self.screen_height:
+                        self.two_pos.remove(position2)
+            if self.times < 2550:
+                self.screen.fill((0+self.times/10, 0, 0))
+            else:
+                self.screen.fill((0 + self.times / 10, 0, 0))
+
+            for position5 in self.five_pos:
+                self.screen.blit(self.five_image, (position5[0], position5[1]))
+            for position2 in self.two_pos:
+                self.screen.blit(self.two_image, (position2[0], position2[1]))
+
+            self.screen.blit(self.student_image, (self.student_pos[0], self.student_pos[1]))
 
 
+            score_scr = self.font.render(f"Ваш счёт: {self.score} пятерок", True, (255, 255, 255))
+            self.screen.blit(score_scr, (10, 10))
+
+            pygame.display.update()
+
+            self.clock.tick(60)
 if __name__ == "__main__":
     Funny_Student()
